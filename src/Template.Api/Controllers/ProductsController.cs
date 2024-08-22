5744d660f7111e0ccfb38664ed544fcbb8c82824
Morasiu <morasiu2@gmail.com>
2024-08-22 11:15:58 +0200

    Tips

--- src/Template.Api/Controllers/ProductsController.cs
+++ src/Template.Api/Controllers/ProductsController.cs
@@ -20,15 +20,14 @@ public class ProductsController : ControllerBase {
 	[HttpGet]
 	[ProducesResponseType(typeof(ICollection<ProductDto>), StatusCodes.Status200OK)]
 	public async Task<ICollection<ProductDto>> GetAll([FromQuery] GetAllProductsQuery request,
-	                                                                CancellationToken cancellationToken) {
+	                                                  CancellationToken cancellationToken) {
 		var result = await _mediator.Send(request, cancellationToken);
 		return result;
 	}
 
 	[HttpPost]
 	[ProducesResponseType(typeof(ProductDto), StatusCodes.Status200OK)]
-	public async Task<ProductDto> Create(CreateProductCommand request,
-	                                                   CancellationToken cancellationToken) {
+	public async Task<ProductDto> Create(CreateProductCommand request, CancellationToken cancellationToken) {
 		var result = await _mediator.Send(request, cancellationToken);
 		return result;
 	}
@@ -36,8 +35,7 @@ public class ProductsController : ControllerBase {
 	[HttpPut("{id:guid}")]
 	[ProducesResponseType(typeof(ProductDto), StatusCodes.Status200OK)]
 	[ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-	public async Task<ProductDto> Update(Guid id, UpdateProductCommand request,
-	                                                   CancellationToken cancellationToken) {
+	public async Task<ProductDto> Update(Guid id, UpdateProductCommand request, CancellationToken cancellationToken) {
 		request.Id = id;
 		var result = await _mediator.Send(request, cancellationToken);
 		return result;
--- src/Template.Persistence/Entities/README.md
+++ src/Template.Persistence/Entities/README.md
@@ -6,3 +6,6 @@ Sample structure
 * `Examples` - note that it should be plural
   * `ExampleEntity.cs` - should have `Entity` suffix to avoid name conflicts
   * `ExampleEntityConfiguration.cs` - should contain all configuration for given entity i.e. key, relations
+
+Notes:
+* Use `ICollection<T>` for navigation properties (EF Core can then use HashSet for then)
